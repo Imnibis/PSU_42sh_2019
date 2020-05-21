0380cc04c24184b805fd0ecabed5e9618bd38d85
Rayane DJELLABI <rayane.djellabi@epitech.eu>
2020-05-21 19:00:48 +0200

    began working on builtins and redirections

--- include/shell.h
+++ include/shell.h
@@ -19,7 +19,14 @@ typedef struct expression {
     char *command;
     int argc;
     char **argv;
+    int pipe_stdout;
+    char *stdin;
 } expression_t;
+
+typedef struct command_return {
+    char *stdout;
+    int return_value;
+} command_return_t;
 
 char *get_command_line(void);
 int display_prompt(dictionary_t *env);
@@ -29,5 +36,8 @@ char *env_get(dictionary_t *env_vars, char *var);
 dictionary_t *env_set(dictionary_t *env_vars, char *index, char *value);
 dictionary_t *env_del(dictionary_t *env_vars, char *index);
 char **env_to_array(dictionary_t *env_vars);
+int cdir(int argc, char **argv, dictionary_t *envp);
+int echo(int argc, char **argv, dictionary_t *envp);
+int exit_shell(int argc, char **argv, dictionary_t *envp);
 
 #endif /* !SHELL_H_ */
