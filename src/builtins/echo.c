7bacb3f77dac12987b44737618baec6aa6193b5d
huntervcx <arthur1.perrot@epitech.eu>
2020-05-23 17:49:17 +0200

    new echo function, totally functionnal

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,58 +6,21 @@
 */
 
 #include "shell.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-char *str_delimit(char *str, int first)
-{
-    char d = '"';
-    int p = 0;
-    int i = first;
-    char *new_str = malloc(sizeof(char) * my_strlen(str) - 2);
-
-    while (str[i] != 0) {
-        if (str[i] != d) {
-            new_str[p] = str[i];
-            p++;
-        }
-        i++;
-    }
-    new_str[p + 1] = 0;
-    return new_str;
-}
-
-void my_putstrb(char *str)
-{
-    int i = 0;
-
-    while (str[i] != 0) {
-        if (str[i] == '\\' && str[i + 1] == 'n') {
-            my_putchar('\n');
-            i = i + 1;
-        }
-        else
-            my_putchar(str[i]);
-        i++;
-    }
-}
 
 int builtin_echo(int argc, char **argv, dictionary_t **env)
 {
-    char *str = "";
-    char arg = 'a';
+    int i = 1;
 
-    if (argc >= 3 && my_strlen(argv[2]) >= 2)
-        arg = argv[2][1];
-    if (argc >= 2)
-        str = argc > 2 ? argv[2] : argv[1];
-    str = str_delimit(str, 0);
-    if (arg == 'n') {
-        my_printf("%s", str);
+    if (argc <= 1) {
+        my_putstr("\n");
+        return 0;
     }
-    else if (arg == 'e')
-        my_putstrb(str);
-    else
-        my_putstr(str);
+    if (my_strcmp(argv[1], "-n") == 0)
+        i = 2;
+    for (i; i < argc; i++) {
+        my_putstr(argv[i]);
+        if (i < argc - 1) my_putstr(" ");
+    }
+    if (my_strcmp(argv[1], "-n") == 1) my_putstr("\n");
     return 0;
 }
