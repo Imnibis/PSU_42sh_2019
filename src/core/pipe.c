3a5d4fb7b293b12be8aa28b440e1a162580a4c0e
huntervcx <arthur1.perrot@epitech.eu>
2020-05-24 19:14:42 +0200

    advance

--- src/core/pipe.c
+++ src/core/pipe.c
@@ -11,9 +11,22 @@ int count_pipes(int argc, char **argv, dictionary_t *env)
 {
     int pipes_number = 0;
 
+    UNUSED(env);
+    UNUSED(argc);
     for(int i = 0; argv[i]; i++){
         if (!my_strcmp("|", argv[i]))
             pipes_number++;
     }
     return pipes_number;
+}
+
+int pipes(int argc, char **argv, dictionary_t *env)
+{
+    int pipefd[2];
+
+    if (pipe(pipefd) == -1) {
+        perror("Pipe error");
+        return 84;
+    }
+    return 0;
 }
